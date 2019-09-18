96c31190eb36ead9b3071256f3339f073b52c1a9
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-18 12:57:43 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(dogs_name)
+    @name = dogs_name 
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def job=(person_job)
+    @job = person_job 
+  end 
+  
+  def job
+    @job
+  end 
+  
+end
