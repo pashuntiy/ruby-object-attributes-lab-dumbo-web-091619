b36efef57fd4892d5289f6015f5333d9c772876a
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-18 12:56:41 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(dogs_name)
+    @name = dogs_name 
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def job=(dogs_breed)
+    @breed = dogs_breed 
+  end 
+  
+  def breed
+    @breed
+  end 
+  
+end
